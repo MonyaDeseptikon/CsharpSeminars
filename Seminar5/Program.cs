6c52bbe697cd8293858dd9ae168c1581fc29379e
Monya <isadom@mail.ru>
2024-02-21 17:48:58 +0400

    Решена последняя обязательная задача Семинара №5

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 
 /*ЗАДАЧА1
+Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет: "Позиция по рядам выходит за пределы массива"
+или
+"Позиция по колонкам выходит за пределы массива"
+Позиции в массиве считать от единицы.
+
 //Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
 class UserInputToCompileForTest
 {
@@ -112,7 +117,9 @@ class Answer
 }
 */
 
-/*ЗАДАЧА2*/
+/*ЗАДАЧА2
+//Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
 //Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
 class UserInputToCompileForTest
 {
@@ -209,6 +216,105 @@ class Answer
                 {5, 6, 7, 8},
                 {9, 10, 11, 12}
             };
+        }
+        UserInputToCompileForTest.PrintResult(numbers);
+    }
+}
+*/
+
+
+
+/*ЗАДАЧА3*/
+//Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{
+    /// Вычисление сумм по строкам (на выходе массив с суммами строк)
+    public static int[] SumRows(int[,] array)
+    {
+        //Напишите свое решение здесь
+        int[] sumRowArray = new int[array.GetLength(0)];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            int count = 0;
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                count += array[i, j];
+            }
+            sumRowArray[i] = count;
+        }
+        return sumRowArray;
+    }
+
+    // Получение индекса минимального элемента в одномерном массиве
+    public static int MinIndex(int[] array)
+    {
+        //Напишите свое решение здесь
+        int min = array[0];
+        int rowNumber = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] < min)
+            {
+                min = array[i];
+                rowNumber = i;
+            }
+        }
+        return rowNumber;
+    }
+    public static void PrintResult(int[,] numbers)
+    {
+        //Напишите свое решение здесь
+        Console.WriteLine(MinIndex(SumRows(numbers)));
+
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[,] numbers;
+
+        if (args.Length >= 1)
+        {
+            // Предполагается, что строки разделены запятой и пробелом, а элементы внутри строк разделены пробелом
+            string[] rows = args[0].Split(',');
+
+            int rowCount = rows.Length;
+            int colCount = rows[0].Trim().Split(' ').Length;
+
+            numbers = new int[rowCount, colCount];
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                string[] rowElements = rows[i].Trim().Split(' ');
+
+                for (int j = 0; j < colCount; j++)
+                {
+                    if (int.TryParse(rowElements[j], out int result))
+                    {
+                        numbers[i, j] = result;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error parsing element {rowElements[j]} to an integer.");
+                        return;
+                    }
+                }
+            }
+        }
+        else
+        {
+            // Если аргументов на входе нет, используем примерный массив
+
+            numbers = new int[,] {
+                {1, 2, 3},
+                {1, 1, 0},
+                {7, 8, 2},
+                {9, 10, 11}
+    };
         }
         UserInputToCompileForTest.PrintResult(numbers);
     }
